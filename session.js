ccb1baa6edd3be6a5d57a2d7331196754faaad5d
Yangamoses <155328009+Yangamoses@users.noreply.github.com>
2024-11-30 02:34:46 +0000

    Update session.js

--- session.js
+++ session.js
@@ -21,5 +21,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "your session_id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-MD=OUVBlRZQ#w16j1APZN0MNbSIcBuBZWgOgUl5BrAie_Dlp8QCEhSA",
 };
